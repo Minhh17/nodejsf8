2184aeb8cce18920b0849f70459e04880560d760
Nguyen Hai Minh <nguyenhaiminhr@gmail.com>
2023-04-15 02:05:58 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const ejs = require('ejs');
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined'));
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+// template engine 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'resourse/views'));
+// console.log('Path: ', path.join(__dirname, 'resourse/views'))
+
+
+//route 
+
+app.get('/', (req, res) => {
+    return res.render('home');
+})
+app.get('/about', (req, res) => {
+    return res.render('about');
+})
+
+app.listen(port, ()=> console.log(`Example app listening on port ${port}`))
